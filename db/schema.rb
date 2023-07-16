8cebddb65d9dc555ccad873893c85b92f00b9063
NNMY03 <135219119+NNMY03@users.noreply.github.com>
2023-07-16 09:40:14 +0000

    [Add]ordersモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_16_092922) do
+ActiveRecord::Schema.define(version: 2023_07_16_093708) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -60,6 +60,19 @@ ActiveRecord::Schema.define(version: 2023_07_16_092922) do
     t.integer "amount", null: false
     t.integer "making_status", default: 0, null: false
     t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "shipping_cost", null: false
+    t.integer "total_payment", null: false
+    t.integer "payment_method", default: 0, null: false
+    t.string "name", null: false
+    t.string "address", null: false
+    t.string "postal_code", null: false
+    t.integer "status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
