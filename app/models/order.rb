7855dd6ffab17d6385945bfa6871f72828dde706
NNMY03 <135219119+NNMY03@users.noreply.github.com>
2023-07-16 12:39:04 +0000

    [Add]商品と注文のアソシエーション追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ApplicationRecord
+  # order_detailsのどのidとアソシエーションするか記述
+ has_many :order_details, class_name: "OrderDetail",foreign_key: "order_id", dependent: :destroy
+# order_detailsが中間テーブルであり実際結びつけるモデルを指定
+ has_many :items, through: :order_details
+
 end
