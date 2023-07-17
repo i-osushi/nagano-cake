41ddfd2401aa0e94e4c42de79d40cd0b3e5a4b2b
NNMY03 <135219119+NNMY03@users.noreply.github.com>
2023-07-17 11:23:07 +0000

    [Add]adrresscontroller追加

---
+++ app/controllers/public/addresses_controller.rb
@@ -0,0 +1,24 @@
+class Public::AddressesController < ApplicationController
+  
+  def index
+    @address = Address.new
+    @addresses = Address.all
+  end
+  
+  def create
+    
+  end
+
+  def edit
+  
+  end
+  
+  def updat
+    
+  end
+  
+  def destroy
+  
+  end
+  
+end
