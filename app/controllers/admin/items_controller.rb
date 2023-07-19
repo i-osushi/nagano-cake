4b503726f91b13f704e1c5c35fde5033fa2bef23
EC2 Default User <ec2-user@ip-172-31-4-234.ec2.internal>
2023-07-19 03:43:58 +0000

    itemのshowとeditページの作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -20,7 +20,7 @@ class Admin::ItemsController < ApplicationController
           flash[:notice] = "Book was successfully updated."
           redirect_to admin_items_path(@item)
         else
-          render 'index'
+          render 'new'
         end
     end
     def edit
@@ -43,7 +43,7 @@ class Admin::ItemsController < ApplicationController
         params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active, :image)
     end
 
-    def set_select_genres
-      @genres = Genre.all.map {|genre| [genre.name, genre.id] }.unshift(["--選択してください--", nil])
-    end
+def set_select_genres
+    @genres = Genre.all.map {|genre| [genre.name, genre.id] }.unshift(["--選択してください--", nil])
+  end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,8 +1,15 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin!
+  
 def show
+  @oders = Oder.find(params[:id])
+  @customer = @oder.customer_id
+  @itme = @oder.item
 end
 
 def update
+  @oder = Oder.find(params[:id])
+  
 end
 
 end
