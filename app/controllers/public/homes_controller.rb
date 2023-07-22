95f6329b64810191d8c3e1c15a5a9d4cde8625c8
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-07-22 06:51:50 +0000

    [Update] top,aboutのレイアウト

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,11 +1,22 @@
 class Public::HomesController < ApplicationController
+  before_action :set_item, only: [:top]
   
   def top
-    
+    # @items = Item.order('id DESC').limit(4)
+    @items = Item.all
+    @items_latest4 = @items.first(4)
   end
   
   def about
     
   end
+  
+  private
+  
+  def set_item
+    # PATHパラメータでitemを取得
+    @item = Item.find_by(params[:item_id])
+  end
+
   
 end
