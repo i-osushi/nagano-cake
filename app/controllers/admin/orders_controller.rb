1a03d7f95633a3230bef8bd5e58ea6955649b457
EC2 Default User <ec2-user@ip-172-31-4-234.ec2.internal>
2023-07-23 15:16:01 +0000

    レイアウト調整

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,25 +1,22 @@
 class Admin::OrdersController < ApplicationController
-
   before_action :authenticate_admin!
 
-  def show
-    @order = Order.find(params[:id])
-    @order_details = @order.order_details
-  end
+def show
+  @order = Order.find(params[:id])
+end
 
-  def update
-    @order = Order.find(params[:id])
-    @order_details = OrderDetail.where(order_id: params[:id])
-    if @order.update(order_params)
-     @order_details.update_all(making_status: 1) if @order.status == "confirm_payment"
-    # ①注文ステータスが「入金確認」とき、製作ステータスを全て「製作待ち」に更新する
-    end
-    redirect_to admin_order_path(@order)
+def update
+  @order = Order.find(params[:id])
+  @order_details = OrderDetail.where(order_id: params[:id])
+  if @order.update(order_params)
+    @order_details.update_all(making_status: 1) if @order.status == "confirm_payment"
+    ## ①注文ステータスが「入金確認」とき、製作ステータスを全て「製作待ち」に更新する
   end
+  redirect_to admin_order_path(@order)
+end
 
 private
-  def order_params
-    params.require(:order).permit(:status)
-  end
-
+def order_params
+  params.require(:order).permit(:status)
+end
 end
