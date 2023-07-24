79c31380c02d7c33c57ca6004346449c2174b821
EC2 Default User <ec2-user@ip-172-31-4-234.ec2.internal>
2023-07-24 09:12:00 +0000

    sute-tasu

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -25,7 +25,7 @@ class Admin::CustomersController < ApplicationController
 
   private
     def customer_params
-      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :phone_number, :is_active)
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :phone_number, :is_deleted)
     end
 
 end
