aa9970018954941d9b33fd9b61fc4ca00d9a2514
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-07-24 14:15:34 +0000

    [Update] my_confirmation

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,7 +2,7 @@ class CartItem < ApplicationRecord
   # 会員・商品：カート内商品= 1 : N
   belongs_to :customer
   belongs_to :item
-  
+
   def get_image(height, width)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -10,13 +10,13 @@ class CartItem < ApplicationRecord
     end
     image.variant(resize_to_limit: [height, width]).processed
   end
-  
+  # 税込価格
   def add_tax_price
     tax = 1.1
     (item.price * tax).round
   end
-  
-  ## 小計を求めるメソッド
+
+  # 小計
   def subtotal
     item.add_tax_price * amount
   end
