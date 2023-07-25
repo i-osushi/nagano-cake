14ba7d293efe4e190cd4ef32551f0ab32bb7bf2c
EC2 Default User <ec2-user@ip-172-31-4-234.ec2.internal>
2023-07-25 08:12:02 +0000

    顧客の商品履歴一覧

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,12 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
 
+def index
+  @customer = Customer.find(params[:customer_id])
+  @orders = @customer.orders
+  @order = Order.all.page(params[:page]).per(10)
+end
+
 def show
   @order = Order.find(params[:id])
 end
